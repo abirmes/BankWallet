ce9a150887d655d6c09e49b70e6f014586ac9b68
abirmes <abir.meskini02@gmail.com>
2025-03-10 12:16:47 +0000

    initializing

---
+++ app/Http/Controllers/helloWorldController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class helloWorldController extends Controller
+{
+    public function index()
+    {
+    return '';
+    }
+}
